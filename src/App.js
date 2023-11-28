c28e913d45860aeb36cba4750700d81bb1bcd0d5
Syed Rabeet-Ur-Rehman <92630941+Rabeet8@users.noreply.github.com>
2023-11-27 21:17:34 -0800

    Trader Box component added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import TraderBox from './components/TraderBox'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+<div className="traders-container">
+        <TraderBox name="Trader Name" amountManaged="N/A Amount managed" apy="N/A APY" />
+        <TraderBox name="Trader Name" amountManaged="N/A Amount managed" apy="N/A APY" />
+        <TraderBox name="Trader Name" amountManaged="N/A Amount managed" apy="N/A APY" />
+        <TraderBox name="Trader Name" amountManaged="N/A Amount managed" apy="N/A APY" />
+      </div>
+  
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/TraderBox.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './TraderBox.css';
+
+const TraderBox = ({ name, amountManaged, apy }) => {
+  return (
+    <div className="trader-box">
+      <div className="trader-name">{name}</div>
+      <div className="trader-info">
+        <div className="amount-managed">{amountManaged}</div>
+        <div className="apy">{apy}</div>
+      </div>
+      <button className="enter-button">Enter</button>
+    </div>
+  );
+};
+
+export default TraderBox;
